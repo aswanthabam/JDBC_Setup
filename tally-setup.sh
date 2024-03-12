f7a3e50d1199e50f8209d47773a72bc59c1070e6
Aswanth Vc <69764494+aswanthabam@users.noreply.github.com>
2024-03-12 13:04:06 +0530

    Create tally-setup.sh

---
+++ tally-setup.sh
@@ -0,0 +1,22 @@
+wget https://tallymirror.tallysolutions.com/download_centre/R6.6.3_GA/TE9/Full/setup.exe
+e_d=$?
+sudo apt install wine
+e_wine=$?
+wine setup.exe
+e_setup=$?
+
+if [ $e_d -ne 0 ];then
+  echo -e "\u001b[31m - Error downloading setup file"
+else
+  echo -e "\u001b[32m + Downloaded setup.exe"
+fi
+if [ $e_wine -ne 0 ];then
+  echo -e "\u001b[31m - Error installing wine"
+else
+  echo -e "\u001b[32m + Installed wine"
+fi
+if [ $e_setup -ne 0 ];then
+  echo -e "\u001b[31m - Error running setup"
+else
+  echo -e "\u001b[32m + Setup started"
+fi
